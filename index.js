73a03a76c2b6e4846a85ce6a1a19063f3d29726f
Laura-fr <laurafraysse84@gmail.com>
2020-11-19 17:15:38 +0100

    cors less

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+
 const publicKey = process.env.PUBLIC_KEY;
 const privateKey = process.env.PRIVATE_KEY;
 
