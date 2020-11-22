586a1b655d71636fbb22e614c59d8985bd074035
Laura-fr <laurafraysse84@gmail.com>
2020-11-22 11:24:51 +0100

    id

--- index.js
+++ index.js
@@ -60,3 +60,23 @@ app.listen(process.env.PORT, () => {
   console.log("server is started");
   //   console.log(`Server Started on port ${process.env.PORT}`);
 });
+
+//ROUTE CHARACTERS ID
+
+app.get("/characters/:id", async (req, res) => {
+  try {
+    const ts = uid2(8);
+    const hash = md5(ts + privateKey + publicKey);
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/characters/${id}?limit=100&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+    );
+    res.json(response.data.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server is started");
+  //   console.log(`Server Started on port ${process.env.PORT}`);
+});
